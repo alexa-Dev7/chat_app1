2a00b2cde70ac2de2f4baa4a870412023125d8c2
alexa-Dev7 <trishnit@outlook.com>
2025-03-21 10:35:50 +0530

    logout.php

--- logout.php
+++ logout.php
@@ -1,5 +1,11 @@
 <?php
 session_start();
+
+// Remove the user's session from session tracking
+$sessions = json_decode(file_get_contents('persistent_data/sessions.json'), true);
+unset($sessions[$_SESSION['username']]);
+file_put_contents('persistent_data/sessions.json', json_encode($sessions));
+
 session_destroy();
 header("Location: index.php");
 exit();
