032ee82f5df6cc3a5c0b416dd08a21d96cf077f3
alexa-Dev7 <trishnit@outlook.com>
2025-03-22 11:04:57 +0530

    db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -6,11 +6,9 @@ $user = getenv('DB_USER') ?: 'chat_app_ltof_user';
 $password = getenv('DB_PASSWORD') ?: 'JtFCFOztPWwHSS6wV4gXbTSzlV6barfq';
 $port = getenv('DB_PORT') ?: 5432;
 
-// PostgreSQL connection
 try {
     $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("❌ Database connection failed: " . $e->getMessage());
 }
-
