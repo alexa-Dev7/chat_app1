7daeee369b1b2176d81bc1aa1879a4e6cfd9237e
alexa-Dev7 <trishnit@outlook.com>
2025-03-25 16:57:02 +0530

    load_chat.php

--- load_chat.php
+++ load_chat.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-header('Content-Type: application/json'); // Ensure JSON response
+header('Content-Type: application/json');
 
 if (!isset($_SESSION['username'])) {
     echo json_encode(["error" => "Unauthorized"]);
@@ -15,13 +15,11 @@ if (empty($user)) {
     exit();
 }
 
-// Determine the correct file
 $filename = "chats/" . $username . "_" . $user . ".json";
 if (!file_exists($filename)) {
     $filename = "chats/" . $user . "_" . $username . ".json";
 }
 
-// Load messages or return empty array
 if (file_exists($filename)) {
     $messages = json_decode(file_get_contents($filename), true);
     echo json_encode(["messages" => $messages ?: []]);
